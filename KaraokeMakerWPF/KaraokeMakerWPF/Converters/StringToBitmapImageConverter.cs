cc1b57616a59063a9a2e9e82c008c21513b8e358
KinGelaim <89123232156@mail.ru>
2025-10-26 12:17:05 +0500

    The background selection page has been improved

---
+++ KaraokeMakerWPF/KaraokeMakerWPF/Converters/StringToBitmapImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace KaraokeMakerWPF.Converters;
+
+public class StringToBitmapImageConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string imagePath && !string.IsNullOrEmpty(imagePath))
+        {
+            try
+            {
+                return new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+            }
+            catch (UriFormatException)
+            {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- KaraokeMakerWPF/KaraokeMakerWPF/ViewModels/SelectBackgroundViewModel.cs
+++ KaraokeMakerWPF/KaraokeMakerWPF/ViewModels/SelectBackgroundViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using KaraokeMakerWPF.Environment;
 using KaraokeMakerWPF.Models;
 using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows.Input;
 
 namespace KaraokeMakerWPF.ViewModels;
@@ -11,18 +13,50 @@ public class SelectBackgroundViewModel : StepByStepViewModelBase
 
     public ICommand SelectImagePathCommand { get; }
 
+    private ObservableCollection<ImageItemData> _images = [];
+    public ObservableCollection<ImageItemData> Images
+    {
+        get => _images;
+        set
+        {
+            _images = value;
+            OnPropertyChanged(nameof(Images));
+        }
+    }
+
     public SelectBackgroundViewModel(KaraokeInfoViewModel karaokeInfoVM)
     {
         KaraokeInfoVM = karaokeInfoVM;
 
         SelectImagePathCommand = new DelegateCommand(SelectImagePath);
+
+        LoadDefaultBackgrounds();
+    }
+
+    private void LoadDefaultBackgrounds()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "Resources\\Backgrounds");
+
+        var files = Directory.GetFiles(path);
+
+        Images.Clear();
+        foreach (var filePath in files)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(filePath);
+            Images.Add(new ImageItemData(fileName, filePath));
+        }
+
+        if (Images.Count > 0)
+        {
+            KaraokeInfoVM.ImageFilePath = Images[0].ImagePath;
+        }
     }
 
     private void SelectImagePath()
     {
         var imageDialog = new OpenFileDialog
         {
-            Filter = "Файлы рисунков (*.png, *.jpg)|*.png;*.jpg"
+            Filter = "Файлы изображений (*.png, *.jpg)|*.png;*.jpg"
         };
 
         if (imageDialog.ShowDialog() == true)
@@ -39,5 +73,17 @@ public class SelectBackgroundViewModel : StepByStepViewModelBase
         }
 
         return StepByStepValidationError.Success();
+    }
+}
+
+public class ImageItemData
+{
+    public string DisplayText { get; set; }
+    public string ImagePath { get; set; }
+
+    public ImageItemData(string displayText, string imagePath)
+    {
+        DisplayText = displayText;
+        ImagePath = imagePath;
     }
 }
